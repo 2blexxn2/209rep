fffb69a38d5754a301ae46b3638cc840081028b3
2blexn <300amfkall@gmail.com>
2024-02-24 14:06:52 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,122 @@
+<template>
+	<div class="about">
+		<h2>Викладачі</h2>
+	</div>
+	<div class="container" style="width: 150%; display: flex; justify-content: center;">
+		<table style="border:1px solid black; border-collapse: collapse; width: 80%;">
+			<tr style="background-color: #212222;">
+				<th style="border:1px solid black;">Ім'я</th>
+				<th style="border:1px solid black;">Telegram</th>
+				<th style="border:1px solid black;">Email</th>
+				<th style="border:1px solid black;">Курси</th>
+				<th style="border:1px solid black;">Посилання</th>
+				<th style="border:1px solid black;">Консультації</th>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Антіпова Катерина Олександрівна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/KaterynaAntipova">@KaterynaAntipova</a></td>
+				<td style="border:1px solid black;"><a
+						href="mailto:antipova.katerina@chmnu.edu.ua">antipova.katerina@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">ОБД, ООП</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/rou-etmu-yrx">Особисте посилання</a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Боровльова Світлана Юріївна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/BSU1952">@BSU1952</a></td>
+				<td style="border:1px solid black;"><a href="mailto:svetlya1952@gmail.com">svetlya1952@gmail.com</a></td>
+				<td style="border:1px solid black;">Мова програмування Python</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/yjv-qxit-wfx">Особисте посилання</a></td>
+				<td style="border:1px solid black;">Вівторок 19:30</td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Буровицька Юлія Миколаївна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/yuliiia11">@yuliiia11</a></td>
+				<td style="border:1px solid black;"><a href="mailto:uliy2005@gmail.com">uliy2005@gmail.com</a></td>
+				<td style="border:1px solid black;">Іноземна мова</td>
+				<td style="border:1px solid black;"><a href=""></a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Гончарова Надія Вікторівна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/NadiiaHoncharova">@NadiiaHoncharova</a></td>
+				<td style="border:1px solid black;"><a
+						href="mailto:honcharova.nadia@chmnu.edu.ua">honcharova.nadia@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">Людино-машинна взаємодія</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/ndm-tdcj-xzy">1. Особисте посилання для
+						лабораторних</a><br>
+					<br><a href="https://meet.google.com/phw-hygv-cpr">2. Особисте посилання для лекційних</a><br>
+					<br><a href="https://meet.google.com/tua-cugi-hxu">3. Особисте посилання для консультацій </a>
+				</td>
+				<td style="border:1px solid black;">щотижня з 15:30 до 16:30</td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Горбань Гліб Валентинович</td>
+				<td style="border:1px solid black;"><a href="https://t.me/gleb_gorban">@gleb_gorban</a></td>
+				<td style="border:1px solid black;"><a href="mailto:hlib.horban@chmnu.edu.ua">hlib.horban@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">ООП</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/ahw-vvsv-syi">Особисте посилання</a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Давиденко Євген Олександрович</td>
+				<td style="border:1px solid black;"><a href="https://t.me/DavydenkoYevhen">@DavydenkoYevhen</a></td>
+				<td style="border:1px solid black;"><a href="mailto:davydenko@chmnu.edu.ua">davydenko@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">SPA</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/cat-tnso-ydp">Особисте посилання</a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Кандиба Ігор Олександрович</td>
+				<td style="border:1px solid black;"><a href="https://t.me/KandybaIhor">@KandybaIhor</a></td>
+				<td style="border:1px solid black;"><a href="mailto:ihor.kandyba@chmnu.edu.ua">ihor.kandyba@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">Мова програмування Python</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/wau-wiwx-rsq">Особисте посилання</a></td>
+				<td style="border:1px solid black;">П'ятниця 18:20</td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Кірей Катерина Олександрівна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/asd0987655">@asd0987655</a></td>
+				<td style="border:1px solid black;"><a href="mailto:kateryna.kirey@chmnu.edu.ua">kateryna.kirey@chmnu.edu.ua</a>
+				</td>
+				<td style="border:1px solid black;">ОБД</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/kjc-mmiz-mhk">Особисте посилання</a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Швед Альона Володимирівна</td>
+				<td style="border:1px solid black;"></td>
+				<td style="border:1px solid black;"><a href="mailto:avshved@chmnu.edu.ua">avshved@chmnu.edu.ua</a></td>
+				<td style="border:1px solid black;">Емпіричні методи програмної інженерії</td>
+				<td style="border:1px solid black;"><a href="https://meet.google.com/aye-qiwa-naq">Особисте посилання</a></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<td style="border:1px solid black;">Шерстюк Людмила Валеріївна</td>
+				<td style="border:1px solid black;"><a href="https://t.me/l1udmy1a">@l1udmy1a</a></td>
+				<td style="border:1px solid black;"><a href="mailto:lvsherstiuk@gmail.com">lvsherstiuk@gmail.com</a></td>
+				<td style="border:1px solid black;">Іноземна мова (англійська)</td>
+				<td style="border:1px solid black;"></td>
+				<td style="border:1px solid black;"></td>
+			</tr>
+
+		</table>
+
+	</div>
+</template>
+
+<style>
+h2{
+	font-family: 'Courier New', Courier, monospace;
+	color: rgb(29, 143, 0);
+	display: flex;
+	justify-content: center;
+}
+table{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+<!-- 	<img src="../assets/pic1.jpg" alt="Girl in a jacket" width="1000" height="600">
+ --></template>
